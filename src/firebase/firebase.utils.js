a7dbde7018144d8b7a2bb6ba0e81f56042dba908
Neil <neil@Neils-MacBook-Air.local>
2020-09-20 01:52:32 +0530

    google auth done

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7v2-9cTVI_GGu_hH7RIccJf1lmj2HSX4",
+  authDomain: "store-34810.firebaseapp.com",
+  databaseURL: "https://store-34810.firebaseio.com",
+  projectId: "store-34810",
+  storageBucket: "store-34810.appspot.com",
+  messagingSenderId: "867823269162",
+  appId: "1:867823269162:web:50f80075be32598a735a53",
+  measurementId: "G-0VMPMC68EB"
+};
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export const signInWithGoogle = () => {
+  auth.signInWithPopup(provider);
+};
