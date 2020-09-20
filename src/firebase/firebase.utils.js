2f70919488e9d532aaec4e235bbf28aa35e56e85
Neil <neil@Neils-MacBook-Air.local>
2020-09-20 17:48:53 +0530

    implemented firebase utils for signin with email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,32 @@ const firebaseConfig = {
   measurementId: "G-0VMPMC68EB"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+  const snapshot = await userRef.get();
+
+  if(!snapshot.exists){
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    }
+    catch (error) {
+      console.log('erroe createg user', error.message)
+    }
+  }
+
+  return userRef;
+}
+
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
